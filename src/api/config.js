d6306a56cac04219bdc82778dca97619ff70b51f
celia <tcc_2007@126.com>
2018-07-18 12:13:49 +0800

    增加文件夹

---
+++ src/api/config.js
@@ -0,0 +1,16 @@
+export const commonParams = {
+  g_tk: 5381,
+  inCharset: 'utf-8',
+  outCharset: 'utf-8',
+  notice: 0,
+  format: 'jsonp'
+}
+
+export const options = {
+  param: 'jsonpCallback'
+  // uin: 0,
+  // platform: h5
+  // needNewCode: 1
+  // _: 1531637752758
+}
+export const ERR_OK = 0
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastclick from 'fastclick'
+
+import 'common/stylus/index.styl'
+
+// 移动设备上的浏览器默认会在用户点击屏幕大约延迟300毫秒后才会触发点击事件，这是为了检查用户是否在做双击。为了能够立即响应用户的点击事件，才有了FastClick。
+fastclick.attach(document.body)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
